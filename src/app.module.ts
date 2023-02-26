4bcf399060d2a2f40aaf4e87f44e7a989f1cb263
unknown <davisperezg@gmail.com>
2023-02-26 02:42:15 -0500

    typeorm, mysql y configurando variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MenuModule } from './menu/menu.module';
 import { ModuleModule } from './module/module.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
 import { ResourceModule } from './resource/resource.module';
@@ -11,22 +10,36 @@ import { AuthModule } from './auth/auth.module';
 import { ResourcesRolesModule } from './resources-roles/resources-roles.module';
 import { ResourcesUsersModule } from './resources-users/resources-users.module';
 import { ServicesUsersModule } from './services-users/services-users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/adm-rpum', {
-      useFindAndModify: false,
-      useCreateIndex: true,
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+      cache: true,
     }),
-    UserModule,
-    RoleModule,
-    ResourceModule,
-    MenuModule,
-    ModuleModule,
-    AuthModule,
-    ResourcesRolesModule,
-    ResourcesUsersModule,
-    ServicesUsersModule,
+    TypeOrmModule.forRoot({
+      type: process.env.TYPE_DATABASE,
+      host: process.env.HOST_DATABASE,
+      port: process.env.PORT_DATABASE,
+      username: process.env.USER_DATABASE,
+      password: process.env.PASSWORD_DATABASE,
+      database: process.env.NAME_DATABASE,
+      entities: [],
+      synchronize: true, //No usar en produccion
+      retryAttempts: 3,
+    } as any),
+    // UserModule,
+    // RoleModule,
+    // ResourceModule,
+    // MenuModule,
+    // ModuleModule,
+    // AuthModule,
+    // ResourcesRolesModule,
+    // ResourcesUsersModule,
+    // ServicesUsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
